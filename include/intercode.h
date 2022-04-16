a45f39e19f4cf6fc9ad31e76696122ad0cd56c81
Chen Lu <181250012@smail.nju.edu.cn>
2022-04-16 09:27:24 +0800

    intercode: fix indent

--- include/intercode.h
+++ include/intercode.h
@@ -7,8 +7,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-// #define TP_INT 1
-// #define TP_FLOAT 2
 #define MAX_INST_NUM 4096
 #define MAX_ENTRY_NUM 2048
 #define MAX_INST_WIDTH 64
@@ -17,92 +15,105 @@ enum EXP_TYPE{EXP_INT = 1, EXP_FLOAT, EXP_VAR, EXP_ADDR}; //exp返回类型
 enum InterType_ {TP_INT=1, TP_FLOAT, TP_ARRAY, TP_FUNC, TP_STRUCTURE}; //数据结构类型
 
 typedef struct VAR_LIST{       // 存储struct内部变量
-    char* name;                //变量名
-    struct VAR_TYPE* var_type; //变量类型
-    struct VAR_LIST* next;     //指针
-    struct VAR_LIST* pre;
-    int offset;                //变量开头位置想对于结构体开始的位置
-    int followed_sz;           //跟随变量大小（方便中间结果计算）
-    int var_id;                //在计算中如果该变量开始位置被载入临时变量，则存储变量id
+  char* name;                //变量名
+  struct VAR_TYPE* var_type; //变量类型
+  struct VAR_LIST* next;     //指针
+  struct VAR_LIST* pre;
+  int offset;                //变量开头位置想对于结构体开始的位置
+  int followed_sz;           //跟随变量大小（方便中间结果计算）
+  int var_id;                //在计算中如果该变量开始位置被载入临时变量，则存储变量id
 }Vlist;
 
 typedef struct VAR_TYPE{       //存储变量的类型信息
-    int type;                  //变量类别：BASIC, ARRAY...
-    int width;
-    union{
-        struct{                
-            // int basic_type;     //对于BASIC 记录为int类型还是float类型
-            int basic_int;      //int类型的值
-            float basic_float;  //float类型的值
-        };
-        /* Type-Array */
-        struct{
-            int sz;             //数组大小
-            int ele_width;      //数组中元素宽度
-            struct VAR_TYPE* next; //数组中元素的类型
-        };
-        /* structure and structure_var */
-        struct{
-            struct VAR_LIST* _vlist;  //结构体中变量链表
-        };
-        /* Type-FUNC */
-        struct{
-            struct VAR_LIST* func_para;  //函数参数
-            struct VAR_TYPE* ret_type;   //函数返回类型
-        };
+  int type;                  //变量类别：BASIC, ARRAY...
+  int width;
+  union{
+    struct{
+      // int basic_type;     //对于BASIC 记录为int类型还是float类型
+      int basic_int;      //int类型的值
+      float basic_float;  //float类型的值
     };
+    /* Type-Array */
+    struct{
+      int sz;             //数组大小
+      int ele_width;      //数组中元素宽度
+      struct VAR_TYPE* next; //数组中元素的类型
+    };
+    /* structure and structure_var */
+    struct{
+      struct VAR_LIST* _vlist;  //结构体中变量链表
+    };
+    /* Type-FUNC */
+    struct{
+      struct VAR_LIST* func_para;  //函数参数
+      struct VAR_TYPE* ret_type;   //函数返回类型
+    };
+  };
 }Vtype;
 
 typedef struct HASH_ENTRY{  //符号表中元素结构（实际上并没有使用hash表实现）
-    char* name;             //符号名
-    int var_id;             //符号开始id (1)定义初始化 (2)stmt初始化
-    struct HASH_ENTRY* next;
-    Vtype* type;            //符号类型 
+  char* name;             //符号名
+  int var_id;             //符号开始id (1)定义初始化 (2)stmt初始化
+  struct HASH_ENTRY* next;
+  Vtype* type;            //符号类型
 }Entry;
 
 typedef struct EXP_RET{     //EXP返回类型，记录EXP解析的信息
-    int _type;              //该EXP数据的类型：常数EXP_INT/FLOAT, 变量id EXP_VAR
-    union{
-        int ival;
-        float fval;
-        struct {
-            char* name;     //好像没有用？
-            Vtype* _vtype;  //对于复杂类型，输出其类型信息
-        };
-        
+  int _type;              //该EXP数据的类型：常数EXP_INT/FLOAT, 变量id EXP_VAR
+  union{
+    int ival;
+    float fval;
+    struct {
+      char* name;     //好像没有用？
+      Vtype* _vtype;  //对于复杂类型，输出其类型信息
     };
-    int var_id;
-    struct INST_LIST* truelist;
-    struct INST_LIST* falselist;
-    // int addr_id;
+  };
+  int var_id;
+  struct INST_LIST* truelist;
+  struct INST_LIST* falselist;
 }Eret;
 
 typedef struct INST_LIST{
-    int inst_id;
-    struct INST_LIST* next;
-    struct INST_LIST* tail;
+  int inst_id;
+  struct INST_LIST* next;
+  struct INST_LIST* tail;
 }Ilist;
-
-// typedef struct STMT_RET{
-//     struct INST_LIST* nextlist;
-// }
 
 // void gen_intercode(Node* root);
 typedef struct INFO{
-    int is_id;
-    int id;
-    int value;
+  int is_id;
+  int id;
+  int value;
 }Info;
 
 typedef struct INSTTYPE{
-    enum{TP_FUNCT=1, TP_LABEL, TP_DEREF, TP_ASSIGN, TP_OP, TP_ADD, TP_SUB, TP_MUL, TP_DIV, TP_ADDR, TP_STAR, TP_GOTO, TP_IF, TP_RETURN, TP_DEC, TP_ARG, TP_CALL, TP_PARAM, TP_READ, TP_WRITE}inst_type;
-    int type;
-    char* name;
-    Info src1;
-    char* op;
-    Info src2;
-    Info dst;
+  enum{TP_FUNCT=1, TP_LABEL, TP_DEREF, TP_ASSIGN, TP_OP, TP_ADD, TP_SUB, TP_MUL, TP_DIV, TP_ADDR, TP_STAR, TP_GOTO, TP_IF, TP_RETURN, TP_DEC, TP_ARG, TP_CALL, TP_PARAM, TP_READ, TP_WRITE}inst_type;
+  int type;
+  char* name;
+  Info src1;
+  char* op;
+  Info src2;
+  Info dst;
 }InstType;
 
+/* all intercode
+LABEL x :               定义标号x
+FUNCTION f :            定义函数f。
+x := y                  赋值操作。
+x := y + z              加法操作。
+x := y - z              减法操作。
+x := y * z              乘法操作。
+x := y / z              除法操作。
+x := &y                 取y的地址赋给x。
+x := *y                 取以y值为地址的内存单元的内容赋给x。
+*x := y                 取y值赋给以x值为地址的内存单元。
+GOTO x                  无条件跳转至标号x。
+IF x [relop] y GOTO z   如果x与y满足[relop]关系则跳转至标号z。
+RETURN x                退出当前函数并返回x值。
+DEC x [size]            内存空间申请，大小为4的倍数。
+ARG x                   传实参x。
+x := CALL f             调用函数，并将其返回值赋给x。
+PARAM                   x函数参数声明。
+*/
 #endif
 
